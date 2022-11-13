2a5d7de7150ca2b351202162e17d6f9e42733ac1
Kevin Rösch <kevin.roesch@kit.edu>
2022-11-13 11:40:30 +0100

    change imports

--- kit_mensa/cli.py
+++ kit_mensa/cli.py
@@ -1,14 +1,13 @@
-import kit_mensa
-
+from .kit_mensa import *
 
 def main():
-    KIT_MENSA_URL = (
+    URL = (
         "https://www.mensaplan.de/karlsruhe/mensa-am-adenauerring/index.html"
     )
-    mensa_webpage_html = kit_mensa.get_mensa_webpage_as_html(KIT_MENSA_URL)
-    mensa_menu_dict = kit_mensa.filter_menus_from_webpage(mensa_webpage_html)
-    weekday_idx = kit_mensa.get_weekday_as_index()
-    mensa_table = kit_mensa.get_menu_for_today(mensa_menu_dict, weekday_idx)
+    mensa_webpage_html = get_mensa_webpage_as_html(URL)
+    mensa_menu_dict = filter_menus_from_webpage(mensa_webpage_html)
+    weekday_idx = get_weekday_as_index()
+    mensa_table = get_menu_for_today(mensa_menu_dict, weekday_idx)
     print(mensa_table)
 
 
